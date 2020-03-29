54c880c87ebe6e97a4ffcc19447c591f36c1e3b9
Jeff Flower <jeff.flower14@gmail.com>
2020-03-29 15:13:09 -0600

    install react bootstrap

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export const App = () => (
   <div>
     <h1>App</h1>
+    <Button variant="outline-primary">Primary</Button>
   </div>
 );
