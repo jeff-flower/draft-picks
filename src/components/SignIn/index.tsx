3a060d00aaaaf9f6a7cb15aeea46165c9b46b705
Jeff Flower <jeff.flower14@gmail.com>
2020-04-06 10:10:46 -0600

    Add sign out component

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-export const SignInPage = () => (
-  <div>
-    <h1>Sign In Page</h1>
-  </div>
-);
+import { SignUpLink } from '../SignUp';
+import { useFirebaseContext } from '../Firebase';
+import { HOME } from '../../constants/routes';
+
+export const SignInPage: React.FC<{}> = () => {
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+  const [error, setError] = React.useState<any>();
+  const history = useHistory();
+  const firebaseContext = useFirebaseContext();
+
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+    firebaseContext
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        setEmail('');
+        setPassword('');
+        history.push(HOME);
+      })
+      .catch((error: any) => {
+        setError(error);
+      });
+  };
+
+  const isInvalid = email === '' || password === '';
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email
+          <input
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
+            type="email"
+          />
+        </label>
+        <label>
+          Password
+          <input
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+            type="password"
+          />
+        </label>
+        <button type="submit" disabled={isInvalid}>
+          Sign In
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+      <SignUpLink />
+    </div>
+  );
+};
