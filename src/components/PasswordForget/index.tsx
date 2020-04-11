3fdbc36b47898da6b0f1b6e3b884ae5a622d21af
Jeff Flower <jeff.flower14@gmail.com>
2020-04-11 14:57:47 -0600

    Add password reset page

--- src/components/PasswordForget/index.tsx
+++ src/components/PasswordForget/index.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { PASSWORD_FORGET } from '../../constants/routes';
+import { useFirebaseContext } from '../Firebase';
 
 export const PasswordForgetPage = () => (
   <div>
     <h1>Password Forget Page</h1>
+    <PasswordForgetForm />
   </div>
+);
+
+const PasswordForgetForm: React.FC<{}> = () => {
+  const [email, setEmail] = React.useState<string>('');
+  const [error, setError] = React.useState<any>(null);
+  const firebase = useFirebaseContext();
+
+  const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    firebase
+      .doPasswordReset(email)
+      .then(() => {
+        setEmail('');
+        setError(null);
+      })
+      .catch((error: any) => {
+        setError(error);
+      });
+  };
+
+  const isInvalid = email === '';
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <button disabled={isInvalid} type="submit">
+        Reset my password
+      </button>
+      {error && <p>{error.message}</p>}
+    </form>
+  );
+};
+
+export const PasswordForgetLink: React.FC<{}> = () => (
+  <p>
+    <Link to={PASSWORD_FORGET}>Forgot password?</Link>
+  </p>
 );
--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { SignUpLink } from '../SignUp';
+import { PasswordForgetLink } from '../PasswordForget';
 import { useFirebaseContext } from '../Firebase';
 import { HOME } from '../../constants/routes';
 
 export const SignInPage: React.FC<{}> = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <SignInForm />
+      <PasswordForgetLink />
+      <SignUpLink />
+    </div>
+  );
+};
+
+const SignInForm: React.FC<{}> = () => {
   const [email, setEmail] = React.useState<string>('');
   const [password, setPassword] = React.useState<string>('');
   const [error, setError] = React.useState<any>();
@@ -29,35 +41,31 @@ export const SignInPage: React.FC<{}> = () => {
   const isInvalid = email === '' || password === '';
 
   return (
-    <div>
-      <h1>Sign In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-            type="email"
-          />
-        </label>
-        <label>
-          Password
-          <input
-            value={password}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPassword(e.target.value)
-            }
-            type="password"
-          />
-        </label>
-        <button type="submit" disabled={isInvalid}>
-          Sign In
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
-      <SignUpLink />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email
+        <input
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+          type="email"
+        />
+      </label>
+      <label>
+        Password
+        <input
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+          type="password"
+        />
+      </label>
+      <button type="submit" disabled={isInvalid}>
+        Sign In
+      </button>
+      {error && <p>{error.message}</p>}
+    </form>
   );
 };
