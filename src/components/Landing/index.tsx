7f1387d4fe9159e9bba18dd948d77675746d254e
Jeff Flower <jeff.flower14@gmail.com>
2020-04-12 17:12:38 -0600

    Move picks from rules page to picks page

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/*
+TODO:
+if not signed in, copy should include mention of sign in, sign up and rules
+if signed in, include mention of rules and picks
+*/
 export const LandingPage = () => (
   <div>
     <h1>Landing</h1>
--- src/components/Rules/index.tsx
+++ src/components/Rules/index.tsx
@@ -1,48 +1,9 @@
 import React from 'react';
 
-import { useFirebaseContext } from '../Firebase';
-import { PicksData } from '../Firebase/firebase';
-
 export const RulesPage = () => {
   return (
     <div>
       <h1>Rules Page</h1>
-    </div>
-  );
-};
-
-const PicksPage: React.FC<{}> = () => {
-  const [players, setPlayers] = React.useState<string[]>([]);
-  const [picks, setPicks] = React.useState<any>([]);
-  const firebaseApi = useFirebaseContext();
-
-  const getPicks = async () => {
-    // TODO: try/catch, check for picksData.hasError
-    const picksData: PicksData = await firebaseApi.getPicksDataForUser();
-
-    if (picksData.playerNames) {
-      setPlayers(picksData.playerNames);
-    }
-
-    if (picksData.picks) {
-      setPicks(picksData.picks);
-    }
-  };
-
-  const savePicks = async () => {
-    // TODO: show success/error message, loading indicator, disable button
-    const result = await firebaseApi.saveUserPicks(picks);
-    if (result.hasError) {
-      console.log(result.error);
-    } else {
-      console.log('success');
-    }
-  };
-
-  return (
-    <div>
-      <button onClick={getPicks}>Get picks data</button>
-      <button onClick={savePicks}>Save picks</button>
     </div>
   );
 };
