b894da2ed9090eed8d0602f1c5e82e93a55988f0
Jeff Flower <jeff.flower14@gmail.com>
2020-04-12 21:08:06 -0600

    Update pick selection

--- src/components/Picks/index.tsx
+++ src/components/Picks/index.tsx
@@ -4,13 +4,11 @@ import Form from 'react-bootstrap/Form';
 import { useFirebaseContext } from '../Firebase';
 import { PicksData } from '../Firebase/firebase';
 import { UserPick } from '../Firebase/util';
-import { SIGN_IN } from '../../constants/routes';
 
 export const PicksPage: React.FC<{}> = () => {
   const [players, setPlayers] = React.useState<string[]>([]);
   const [picks, setPicks] = React.useState<UserPick[]>([]);
   const [loading, setLoading] = React.useState<boolean>(false);
-  const [loadingError, setLoadingError] = React.useState<boolean>(false);
   const firebaseApi = useFirebaseContext();
 
   React.useEffect(() => {
@@ -21,7 +19,7 @@ export const PicksPage: React.FC<{}> = () => {
       const picksData: PicksData = await firebaseApi.getPicksDataForUser();
 
       if (picksData.playerNames) {
-        setPlayers(picksData.playerNames);
+        setPlayers(['', ...picksData.playerNames]);
       }
 
       if (picksData.picks) {
@@ -33,6 +31,21 @@ export const PicksPage: React.FC<{}> = () => {
 
     getPicks();
   }, [firebaseApi]);
+
+  const handlePickSelection = (newPick: UserPick) => {
+    setPicks((oldPicks) => {
+      const updatedPicks = [...oldPicks];
+      const replaceIndex = updatedPicks.findIndex(
+        (currentPick) => currentPick.pickNumber === newPick.pickNumber
+      );
+      if (replaceIndex !== -1) {
+        updatedPicks.splice(replaceIndex, 1, newPick);
+      } else {
+        console.log('could not find matching pick');
+      }
+      return updatedPicks;
+    });
+  };
 
   const savePicks = async () => {
     // TODO: show success/error message, loading indicator, disable button
@@ -48,10 +61,13 @@ export const PicksPage: React.FC<{}> = () => {
     <div>
       <h1>Your Picks</h1>
       {loading && <p>loading picks data...</p>}
-      {loadingError && <p>Oh boy, something went wrong</p>}
-      {!loading && !loadingError && picks && (
+      {!loading && picks && (
         <Form>
-          <PicksDropdowns picks={picks} players={players} />
+          <PicksDropdowns
+            picks={picks}
+            players={players}
+            onPlayerPicked={handlePickSelection}
+          />
         </Form>
       )}
     </div>
@@ -59,8 +75,7 @@ export const PicksPage: React.FC<{}> = () => {
 };
 
 /*
-picks form should be dumb
-pass players, picks, callback for updating pick, callback for saving
+handle selection
 button should be disabled on submit
 showing loading indicator while fetching picks data
 <form>
@@ -69,17 +84,30 @@ showing loading indicator while fetching picks data
 </form>
 */
 
-const PicksDropdowns: React.FC<{ picks: UserPick[]; players: string[] }> = ({
-  picks,
-  players,
-}) => {
+const PicksDropdowns: React.FC<{
+  picks: UserPick[];
+  players: string[];
+  onPlayerPicked: (pick: UserPick) => void;
+}> = ({ picks, players, onPlayerPicked }) => {
   const playerOptions = players.map((player, index) => (
-    <option key={index}>{player}</option>
+    <option key={index} value={player}>
+      {player}
+    </option>
   ));
   const dropdowns = picks.map(({ pickNumber, pick, team }) => (
     <Form.Group key={pickNumber} controlId={`picksDropdown${pickNumber}`}>
       <Form.Label>{`${pickNumber}. ${team}`}</Form.Label>
-      <Form.Control as="select">{playerOptions}</Form.Control>
+      <Form.Control
+        as="select"
+        value={pick}
+        onChange={(e: any) => {
+          e.preventDefault();
+          const pick: UserPick = { pickNumber, team, pick: e.target.value };
+          onPlayerPicked(pick);
+        }}
+      >
+        {playerOptions}
+      </Form.Control>
     </Form.Group>
   ));
 
