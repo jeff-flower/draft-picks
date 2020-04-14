e7a0a83c8d8ce0374209b116387e24f5646b6e59
Jeff Flower <jeff.flower14@gmail.com>
2020-04-13 19:47:41 -0600

    Add directiosn on home page

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-/*
-TODO:
-if not signed in, copy should include mention of sign in, sign up and rules
-if signed in, include mention of rules and picks
-*/
-export const LandingPage = () => (
-  <div>
-    <h1>Landing</h1>
-  </div>
+import { useUserSession } from '../UserSession';
+import { PICKS, SIGN_IN, SIGN_UP, RULES } from '../../constants/routes';
+
+export const LandingPage = () => {
+  const user = useUserSession();
+
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      {user ? <SignedInLanding /> : <NotSignedInLanding />}
+    </div>
+  );
+};
+
+const SignedInLanding = () => (
+  <>
+    <p>
+      Welcome back! Need a reminder how this works? Check out the <RulesLink />
+    </p>
+    <p>
+      Think rules are for sissies? Start making your <PicksLink />!
+    </p>
+  </>
 );
+
+const NotSignedInLanding = () => (
+  <>
+    <p>
+      Returning player? Head here to <SignInLink />
+    </p>
+    <p>
+      Need a refresher before you get started? Check out the <RulesLink />
+    </p>
+    <p>
+      New player? <SignUpLink />!
+    </p>
+  </>
+);
+
+const SignInLink = () => (
+  <Link to={SIGN_IN}>sign in or reset your password</Link>
+);
+const RulesLink = () => <Link to={RULES}>rules</Link>;
+const PicksLink = () => <Link to={PICKS}>picks</Link>;
+const SignUpLink = () => <Link to={SIGN_UP}>Sign up</Link>;
