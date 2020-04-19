6c27fe81816564e28621d46affaade4429631846
Jeff Flower <jeff.flower14@gmail.com>
2020-04-19 10:14:54 -0600

    move nav items to dropdown

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import { SignOutButton } from '../SignOut';
 import * as ROUTES from '../../constants/routes';
@@ -11,43 +13,44 @@ export const Navigation: React.FC<{}> = () => {
   const user = useUserSession();
   return (
     <nav className="navheader">
-      {user ? <AuthorizedUserNavigation /> : <UnauthorizedUserNavigation />}
+      <DropdownButton variant="secondary" title="Menu" id="dropdownMenu">
+        {user ? <AuthorizedUserNavigation /> : <UnauthorizedUserNavigation />}
+      </DropdownButton>
     </nav>
   );
 };
 
 const AuthorizedUserNavigation: React.FC<{}> = () => (
   <>
-    <ul>
-      <li>
-        <Link to={ROUTES.LANDING}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.RULES}>Rules</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.PICKS}>Picks</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.TRADES}>Trades</Link>
-      </li>
-      <li className="signout">
-        <SignOutButton />
-      </li>
-    </ul>
+    <Dropdown.Item>
+      <Link to={ROUTES.LANDING}>Home</Link>
+    </Dropdown.Item>
+    <Dropdown.Item>
+      <Link to={ROUTES.RULES}>Rules</Link>
+    </Dropdown.Item>
+    <Dropdown.Item>
+      <Link to={ROUTES.PICKS}>Picks</Link>
+    </Dropdown.Item>
+    <Dropdown.Item>
+      <Link to={ROUTES.TRADES}>Trades</Link>
+    </Dropdown.Item>
+    <Dropdown.Divider />
+    <Dropdown.Item>
+      <SignOutButton />
+    </Dropdown.Item>
   </>
 );
 
 const UnauthorizedUserNavigation: React.FC<{}> = () => (
-  <ul>
-    <li>
+  <>
+    <Dropdown.Item>
       <Link to={ROUTES.LANDING}>Home</Link>
-    </li>
-    <li>
+    </Dropdown.Item>
+    <Dropdown.Item>
       <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
-    <li>
+    </Dropdown.Item>
+    <Dropdown.Item>
       <Link to={ROUTES.RULES}>Rules</Link>
-    </li>
-  </ul>
+    </Dropdown.Item>
+  </>
 );
