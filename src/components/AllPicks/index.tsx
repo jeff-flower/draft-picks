d08613f9f2ab9bffdaf9e73690c4d2e849237a67
Jeff Flower <jeff.flower14@gmail.com>
2020-04-22 21:49:46 -0600

    Add all picks table

--- src/components/AllPicks/index.tsx
+++ src/components/AllPicks/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
 import { useFirebaseContext } from '../Firebase';
 import { PicksSummary } from '../Firebase/firebase';
@@ -19,6 +20,41 @@ export const AllPicksPage: React.FC<{}> = () => {
   return (
     <div>
       <h1>All Picks</h1>
+      {!allPicks.length && <p>loading all picks...</p>}
+      {!!allPicks.length && (
+        <Table striped bordered variant="dark" responsive>
+          <thead>
+            <tr>
+              <td>Player</td>
+              {Array.from(new Array(32), (x, i) => i).map((val) => (
+                <td key={val}>{val + 1}</td>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {allPicks.map((userPicksSummary) => (
+              <PicksRow
+                key={userPicksSummary.username}
+                picksSummary={userPicksSummary}
+              />
+            ))}
+          </tbody>
+        </Table>
+      )}
     </div>
+  );
+};
+
+const PicksRow: React.FC<{ picksSummary: PicksSummary }> = ({
+  picksSummary,
+}) => {
+  const { username, orderedPicks } = picksSummary;
+  return (
+    <tr>
+      <td>{username}</td>
+      {orderedPicks.map((playerName, index) => (
+        <td key={`${username}-pick${index}`}>{playerName}</td>
+      ))}
+    </tr>
   );
 };
